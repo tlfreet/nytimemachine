10390e28c21e2edf53461255e8ae9fcada17185b
tlfreet <tfreet@gmail.com>
2018-01-10 09:29:37 -0500

    form working

--- index.js
+++ index.js
@@ -7,19 +7,21 @@ const STORE= {
 }
 
 function inputSearchTerms (){
-    $('#submit-form').submit(function(event) {
+     console.log('inputSearchTerms ran');
+     $('#submit-form').submit(function(event) {
+        alert('SUBMIT ran');
         event.preventDefault();
-        console.log('inputSearchTerms ran');
+        $('#submit-button').change(function(){
+        console.log(this.value);        
+       // var inputDate = new Date(this.value);
+    });
+      // console.log(year);
 
-        //const year = $('#submit-button').val();
-       // console.log(year);
-
-    }
-
-    )
+         })
 }
 
 function getDataFromApi(year, month, day, callback){
+    inputSearchTerms();
     var NYTIIMES_URL = `https://api.nytimes.com/svc/archive/v1/${year}/${month}.json`;
     NYTIIMES_URL+= '?' + $.param({
         'api-key': "a2df3a93ff5541dd979083ae7243e85c"
@@ -45,13 +47,13 @@ function getDataFromApi(year, month, day, callback){
         {if (page.type_of_material == 'Front Page' && page.pub_date == `1972-08-01T00:00:00Z`) {
             return ( `<div class="article">
             <a href=${page.web_url}> ${page.headline.main}</a>
-            <img src=${page.multimedia.url}>
             <p>${page.snippet}</p>
             </div>`)
            
             //renderSearchResults();
             //console.log(page.headline)}});
       // multimedia.pub_date = `${year}-0${month}-${day}T00:00:00Z`
+         //<img src=${page.multimedia.url}>
         };
      });
      console.log(allPages);
@@ -76,5 +78,5 @@ function getDataFromApi(year, month, day, callback){
 
 
 
-$(inputSearchTerms());
+//$(inputSearchTerms());
 $(getDataFromApi (1972, 8, handleApiResults));
