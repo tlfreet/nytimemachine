be3688498feb004e1f2041086fc05f0457aa79a7
tlfreet <tfreet@gmail.com>
2018-01-18 14:02:10 -0500

    using article search api

--- index.js
+++ index.js
@@ -7,70 +7,60 @@ const STORE= {
 }
 
 function inputSearchTerms (){
-     console.log('inputSearchTerms ran');
      $('#submit-form').submit(function(event) {
-        alert('SUBMIT ran');
         event.preventDefault();
-        $('#submit-button').change(function(){
-        console.log(this.value);        
-       // var inputDate = new Date(this.value);
-    });
-      // console.log(year);
+        //let inputDate = $('#date').val();
+        var date = new Date($('#date').val());
+        day = date.getDate() + 1; 
+        month = date.getMonth() + 1; 
+        year = date.getFullYear(); 
+       fullSearchDate = [year, month, day].join("");
+       console.log(fullSearchDate);
+        getDataFromApi(fullSearchDate, fullSearchDate, handleApiResults);
 
          })
 }
 
-function getDataFromApi(year, month, day, callback){
-    inputSearchTerms();
-    var NYTIIMES_URL = `https://api.nytimes.com/svc/archive/v1/${year}/${month}.json`;
-    NYTIIMES_URL+= '?' + $.param({
-        'api-key': "a2df3a93ff5541dd979083ae7243e85c"
+function getDataFromApi(begin_date, end_date, callback){
+    //inputSearchTerms();
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+        'api-key': "a2df3a93ff5541dd979083ae7243e85c",
+        'begin_date': begin_date,
+        'end_date': end_date,
     });
     $.ajax({
-         url: NYTIIMES_URL,
+         url: url,
          method: 'GET',
-         success: handleApiResults
-    }).done(function(results) {
-
+    }).done(function(result) {
+        console.log(result);
     }).fail(function(err) {
         throw err;
-    });
-   STORE.searchYear = year;
-   STORE.searchMonth = month;
-  //  Store.searchDay = day;
-}
+        });
+    }
 
  function handleApiResults (data){
      console.log('handleApiResults ran');
-     const allPages = data.response.docs;
+     /*const allPages = data.response.docs;
+     console.log(`${year}-${month}-${day}T00:00:00Z`);
      const pageOne = allPages.map(function(page) 
-        {if (page.type_of_material == 'Front Page' && page.pub_date == `1972-08-01T00:00:00Z`) {
+        {if (page.pub_date == `${year}-${month}-${day}T00:00:00Z`) {
             return ( `<div class="article">
             <a href=${page.web_url}> ${page.headline.main}</a>
             <p>${page.snippet}</p>
             </div>`)
-           
-            //renderSearchResults();
-            //console.log(page.headline)}});
-      // multimedia.pub_date = `${year}-0${month}-${day}T00:00:00Z`
-         //<img src=${page.multimedia.url}>
+        }
+         else {
+             console.log(page.pub_date);
+             
         };
-     });
+     }); 
      console.log(allPages);
-      $(".past-news").html(pageOne);
-     //STORE.results = pageOne;
+ */     
+    
 }
 
-//function sortByDay (data){}
 
-//render the data from api
-/*will want
-    for url: docs.web_url
-    for title: docs.headline.main
-    for image: docs.multimedia.url
-    for blurb: docs.snippet
-    for page: docs.multimedia.print_page
-*/ 
 
 
 
@@ -78,5 +68,5 @@ function getDataFromApi(year, month, day, callback){
 
 
 
-//$(inputSearchTerms());
-$(getDataFromApi (1972, 8, handleApiResults));
+$(inputSearchTerms());
+//$(getDataFromApi ('19720801', '19720801', handleApiResults));
